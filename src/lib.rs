836a3a37d83a4f1ed76ec5933e414c6b556fbdc4
cohenarthur <arthur.cohen@epita.fr>
2020-05-02 00:03:42 +0200

    doc: Add crate description

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+//! `stir` (or Small Threaded Intermediate Representation) is an intermediate
+//! representation providing JIT Compilation and an interface to LLVM IR.
+//! It aims to improve code speed by analyzing and multithreading code units.
+//!
+//! `stir` is organized in blocks. The smaller the block, the easier to
+//! multithread !
+
 #[cfg(test)]
 mod tests {
     #[test]
