c7a84e4eafccd5dc9fee308bc9ab3e9f85b862b5
cohenarthur <arthur.cohen@epita.fr>
2020-05-03 05:38:34 +0200

    critical: Add more documentation

--- src/blocks/critical.rs
+++ src/blocks/critical.rs
@@ -1,4 +1,5 @@
-//! A Critical block is a block that shall not be multithreaded.
+//! A Critical block is a block that shall not be multithreaded. Critical blocks
+//! wrap around any kind of block.
 
 use super::BasicBlock;
 
@@ -10,7 +11,7 @@ pub struct Critical<'block> {
 }
 
 impl <'block> Critical<'block> {
-    /// Create a new Critical block and wrap it around another block
+    /// Create a new Critical block and wrap it around another block.
     /// When noticing a critical block in the syntax, create said block and
     /// then wrap it in a Critical block.
     pub fn new(block: &'block dyn BasicBlock) -> Critical<'block> {
